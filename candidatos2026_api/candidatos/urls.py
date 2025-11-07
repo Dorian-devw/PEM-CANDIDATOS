699447402ff5b92be39b7e169d53ed057f126720
Dorian-devw <frank.sinca@tecsup.edu.pe>
2025-11-07 10:47:51 -0500

    urls to send routes

--- candidatos2026_api/candidatos/urls.py
+++ candidatos2026_api/candidatos/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import CandidatoViewSet
+from .views import CandidatoViewSet, ProyectoViewSet, DenunciaViewSet
 
 router = DefaultRouter()
-router.register(r'candidatos', CandidatoViewSet)
+router.register('candidatos', CandidatoViewSet)
+router.register('proyectos', ProyectoViewSet)
+router.register('denuncias', DenunciaViewSet)
 
 urlpatterns = router.urls
