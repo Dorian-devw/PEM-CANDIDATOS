edda033f9d09a24bae187755c4e5d254ce158d39
Dorian-devw <frank.sinca@tecsup.edu.pe>
2025-11-07 10:44:43 -0500

    serializers.py to proyecto, denuncia and candidato

--- candidatos2026_api/candidatos/serializers.py
+++ candidatos2026_api/candidatos/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from .models import Candidato
+from .models import Candidato, Proyecto, Denuncia
+
+class ProyectoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Proyecto
+        fields = '__all__'
+
+class DenunciaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Denuncia
+        fields = '__all__'
 
 class CandidatoSerializer(serializers.ModelSerializer):
+    proyectos = ProyectoSerializer(many=True, read_only=True)
+    denuncias = DenunciaSerializer(many=True, read_only=True)
+
     class Meta:
         model = Candidato
         fields = '__all__'
