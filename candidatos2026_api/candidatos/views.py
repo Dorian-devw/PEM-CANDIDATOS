eee23724bde51e9a7398835ab8b4f1b3bf298e8d
Dorian-devw <frank.sinca@tecsup.edu.pe>
2025-11-07 10:46:07 -0500

    views.py beetween serializers.py to send proyecto, candidato and denuncia

--- candidatos2026_api/candidatos/views.py
+++ candidatos2026_api/candidatos/views.py
@@ -1,7 +1,15 @@
 from rest_framework import viewsets
-from .models import Candidato
-from .serializers import CandidatoSerializer
+from .models import Candidato, Proyecto, Denuncia
+from .serializers import CandidatoSerializer, ProyectoSerializer, DenunciaSerializer
 
 class CandidatoViewSet(viewsets.ModelViewSet):
     queryset = Candidato.objects.all()
     serializer_class = CandidatoSerializer
+
+class ProyectoViewSet(viewsets.ModelViewSet):
+    queryset = Proyecto.objects.all()
+    serializer_class = ProyectoSerializer
+
+class DenunciaViewSet(viewsets.ModelViewSet):
+    queryset = Denuncia.objects.all()
+    serializer_class = DenunciaSerializer
